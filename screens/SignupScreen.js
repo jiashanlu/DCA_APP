13450f2c58bcf16316e8fb8b7fab2050e7c48c60
jiashanlu <benjaminbois@gmail.com>
2021-07-11 22:57:37 +0400

    ok

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { TouchableWithoutFeedback, Keyboard } from "react-native";
 import {
-  NativeBaseProvider,
   Box,
   Heading,
   VStack,
@@ -11,82 +11,87 @@ import {
   Icon,
   IconButton,
   HStack,
-} from 'native-base';
+  KeyboardAvoidingView,
+} from "native-base";
 
 export default function SignupScreen() {
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Box flex={1} p={2} w="90%" mx="auto" justifyContent="center" safeArea>
+        <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={100}>
+          <Heading size="lg" color="primary.500">
+            Welcome
+          </Heading>
+          <Heading color="muted.400" size="xs">
+            Sign up to continue!
+          </Heading>
 
- return (
-      <Box
-        flex={1}
-        p={2}
-        w="90%"
-        mx='auto'
-      >
-        <Heading size="lg" color='primary.500'>
-          Welcome
-        </Heading>
-        <Heading color="muted.400" size="xs">
-          Sign up to continue!
-        </Heading>
-
-        <VStack space={2} mt={5}>
-          <FormControl>
-            <FormControl.Label _text={{color: 'muted.700', fontSize: 'sm', fontWeight: 600}}>
+          <VStack space={2} mt={5}>
+            <FormControl>
+              <FormControl.Label
+                _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
+              >
                 Email
-            </FormControl.Label>
-            <Input />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label  _text={{color: 'muted.700', fontSize: 'sm', fontWeight: 600}}>
+              </FormControl.Label>
+              <Input />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label
+                _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
+              >
                 Password
-            </FormControl.Label>
-            <Input type="password" />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label  _text={{color: 'muted.700', fontSize: 'sm', fontWeight: 600}}>
-               Confirm Password
-            </FormControl.Label>
-            <Input type="password" />
-          </FormControl>
-          <VStack  space={2}  mt={5}>
-          <Button colorScheme="cyan" _text={{color: 'white' }}>
-              SignUp
-          </Button>
+              </FormControl.Label>
+              <Input type="password" />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label
+                _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
+              >
+                Confirm Password
+              </FormControl.Label>
+              <Input type="password" />
+            </FormControl>
+            <VStack space={2} mt={5}>
+              <Button colorScheme="cyan" _text={{ color: "white" }}>
+                SignUp
+              </Button>
 
-<HStack justifyContent="center" alignItem='center' >
-          <IconButton
-            variant='unstyled'
-            startIcon={
-              <Icon
-                as={< MaterialCommunityIcons name="facebook" />}
-                color='muted.700'
-                size='sm'
-              />
-            }
-          />
-          <IconButton
-            variant='unstyled'
-            startIcon={
-              <Icon
-                as={< MaterialCommunityIcons name="google" />}
-                color='muted.700'
-                size="sm"
-              />
-            }
-          />
-          <IconButton
-            variant='unstyled'
-            startIcon={
-              <Icon
-                as={< MaterialCommunityIcons name="github" />}
-                color='muted.700'
-                size="sm"
-              />
-            }
-          />
-          </HStack>
+              <HStack justifyContent="center" alignItem="center">
+                <IconButton
+                  variant="unstyled"
+                  startIcon={
+                    <Icon
+                      as={<MaterialCommunityIcons name="facebook" />}
+                      color="muted.700"
+                      size="sm"
+                    />
+                  }
+                />
+                <IconButton
+                  variant="unstyled"
+                  startIcon={
+                    <Icon
+                      as={<MaterialCommunityIcons name="google" />}
+                      color="muted.700"
+                      size="sm"
+                    />
+                  }
+                />
+                <IconButton
+                  variant="unstyled"
+                  startIcon={
+                    <Icon
+                      as={<MaterialCommunityIcons name="github" />}
+                      color="muted.700"
+                      size="sm"
+                    />
+                  }
+                />
+              </HStack>
+            </VStack>
           </VStack>
-        </VStack>
+        </KeyboardAvoidingView>
       </Box>
+    </TouchableWithoutFeedback>
   );
 }
