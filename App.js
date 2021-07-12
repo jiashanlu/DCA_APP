b6b8cd7c6dff36752a88a228b6439875ccd68fe1
jiashanlu <benjaminbois@gmail.com>
2021-07-12 22:31:06 +0400

    ok

--- App.js
+++ App.js
@@ -1,26 +1,57 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NativeBaseProvider, extendTheme } from "native-base";
+import { NativeBaseProvider, extendTheme, themeTools } from "native-base";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
-import { Appearance } from 'react-native';
-import { DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { Appearance } from "react-native";
+import { DefaultTheme, DarkTheme } from "@react-navigation/native";
 
 const Stack = createStackNavigator();
-const colorScheme = Appearance.getColorScheme();
 
-function App() {
-  return (
-    <NativeBaseProvider>
-      <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={LoginScreen} />
-          <Stack.Screen name="Signup" component={SignupScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
-  );
+class App extends React.Component {
+  colorScheme = Appearance.getColorScheme();
+
+  state = {
+    colorMode: this.colorScheme,
+  };
+
+  setColorScheme = (colorScheme) => {
+    this.setState({ colorMode: colorScheme });
+  };
+
+  listener = (Appearance.AppearanceListener = (
+    { colorScheme } /* <-- ignore */
+  ) => {
+    this.setColorScheme(colorScheme);
+    console.log(colorScheme);
+  });
+
+  initAppearanceListener = () => {
+    Appearance.addChangeListener(this.listener);
+  };
+
+  componentDidMount = () => {
+    this.initAppearanceListener();
+  };
+
+  componentWillUnmount = () => {
+    Appearance.removeChangeListener(this.listener);
+  };
+
+  render() {
+    return (
+      <NativeBaseProvider>
+        <NavigationContainer
+          theme={this.state.colorMode === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    );
+  }
 }
-
 export default App;
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,9 @@
 import * as React from "react";
-import { TouchableOpacity } from "react-native";
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import {
   Box,
@@ -13,7 +17,9 @@ import {
   Icon,
   IconButton,
   HStack,
+  KeyboardAvoidingView,
 } from "native-base";
+
 
 export default function LoginScreen({ navigation }) {
   const handleSignupButton = () => {
@@ -21,86 +27,91 @@ export default function LoginScreen({ navigation }) {
   };
 
   return (
-    <Box flex={1} p={2} w="90%" mx="auto">
-      <Heading size="lg" color="primary.500">
-        Welcome
-      </Heading>
-      <Heading color="muted.400" size="xs">
-        Sign in to continue!
-      </Heading>
-      <VStack space={2} mt={5}>
-        <FormControl>
-          <FormControl.Label
-            _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
-          >
-            Email ID
-          </FormControl.Label>
-          <Input />
-        </FormControl>
-        <FormControl mb={5}>
-          <FormControl.Label
-            _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
-          >
-            Password
-          </FormControl.Label>
-          <Input type="password" />
-          <Link
-            _text={{ fontSize: "xs", fontWeight: "700", color: "cyan.500" }}
-            alignSelf="flex-end"
-            mt={1}
-          >
-            Forget Password?
-          </Link>
-        </FormControl>
-        <VStack space={2}>
-          <Button colorScheme="cyan" _text={{ color: "white" }}>
-            Login
-          </Button>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Box flex={1} p={2} w="90%" mx="auto" justifyContent="center" safeArea>
+        <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={100}>
+          <Heading size="lg" color="primary.500">
+            Welcome
+          </Heading>
+          <Heading color="muted.400" size="xs">
+            Sign in to continue!
+          </Heading>
+          <VStack space={2} mt={5}>
+            <FormControl
+              // _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
+            >
+              <FormControl.Label>Email ID</FormControl.Label>
+              <Input 
+               _light={{color: 'red' }}
+               _dark={{color: 'blue' }}
+               />
+            </FormControl>
+            <FormControl mb={5}>
+              <FormControl.Label
+                _text={{ color: "muted.700", fontSize: "sm", fontWeight: 600 }}
+              >
+                Password
+              </FormControl.Label>
+              <Input type="password" />
+              <Link
+                _text={{ fontSize: "xs", fontWeight: "700", color: "cyan.500" }}
+                alignSelf="flex-end"
+                mt={1}
+              >
+                Forget Password?
+              </Link>
+            </FormControl>
+            <VStack space={2}>
+              <Button colorScheme="cyan" _text={{ color: "white" }}>
+                Login
+              </Button>
 
-          <HStack justifyContent="center" alignItem="center">
-            <IconButton
-              variant="unstyled"
-              startIcon={
-                <Icon
-                  as={<MaterialCommunityIcons name="facebook" />}
-                  color="muted.700"
-                  size="sm"
+              <HStack justifyContent="center" alignItem="center">
+                <IconButton
+                  variant="unstyled"
+                  startIcon={
+                    <Icon
+                      as={<MaterialCommunityIcons name="apple" />}
+                      color="muted.700"
+                      size="sm"
+                    />
+                  }
                 />
-              }
-            />
-            <IconButton
-              variant="unstyled"
-              startIcon={
-                <Icon
-                  as={<MaterialCommunityIcons name="google" />}
-                  color="muted.700"
-                  size="sm"
+                <IconButton
+                  variant="unstyled"
+                  startIcon={
+                    <Icon
+                      as={<MaterialCommunityIcons name="twitter" />}
+                      color="muted.700"
+                      size="sm"
+                    />
+                  }
                 />
-              }
-            />
-            <IconButton
-              variant="unstyled"
-              startIcon={
-                <Icon
-                  as={<MaterialCommunityIcons name="github" />}
-                  color="muted.700"
-                  size="sm"
+                <IconButton
+                  variant="unstyled"
+                  startIcon={
+                    <Icon
+                      as={<MaterialCommunityIcons name="google" />}
+                      color="muted.700"
+                      size="sm"
+                    />
+                  }
                 />
-              }
-            />
-          </HStack>
-        </VStack>
-        <HStack justifyContent="center">
-          <Text fontSize="sm" color="muted.700" fontWeight={400}>
-            I'm a new user.{" "}
-          </Text>
-          <TouchableOpacity onPress={handleSignupButton}>
-            <Text color="cyan.500" bold={true} fontSize="sm">
-              Sign Up
-            </Text>
-          </TouchableOpacity>
-        </HStack>
-      </VStack>
-    </Box>
+              </HStack>
+            </VStack>
+            <HStack justifyContent="center">
+              <Text fontSize="sm" color="muted.700" fontWeight={400}>
+                I'm a new user.{" "}
+              </Text>
+              <TouchableOpacity onPress={handleSignupButton}>
+                <Text color="cyan.500" bold={true} fontSize="sm">
+                  Sign Up
+                </Text>
+              </TouchableOpacity>
+            </HStack>
+          </VStack>
+        </KeyboardAvoidingView>
+      </Box>
+    </TouchableWithoutFeedback>
   );
 }
